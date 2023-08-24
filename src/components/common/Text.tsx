c966f722ee8b101088e7df88392b9673605c1522
ljh0608 <m98m@catholic.ac.kr>
2023-08-24 22:59:02 +0900

    style: line-height, head4 추가

---
+++ src/components/common/Text.tsx
@@ -0,0 +1,52 @@
+import { styled } from 'styled-components';
+import { theme } from '../../styles/theme';
+
+interface ValueProps {
+  children: string | string[];
+  font: string;
+  color?: string;
+}
+
+const Text = ({ children, font, color }: ValueProps) => {
+  return (
+    <TextWrapper $font={font} $color={color || 'defaultColor'}>
+      {children}
+    </TextWrapper>
+  );
+};
+
+const TextWrapper = styled.span<{ $font: string; $color: string }>`
+  display: flex;
+  align-items: center;
+
+  word-break: keep-all;
+  color: ${({ $color }) => $color};
+  ${({ $font, theme }) => {
+    switch ($font) {
+      case 'head1':
+        return theme.fonts.head1;
+      case 'head2':
+        return theme.fonts.head2;
+      case 'head3':
+        return theme.fonts.head3;
+      case 'head4':
+        return theme.fonts.head4;
+      case 'name1':
+        return theme.fonts.name1;
+      case 'name2':
+        return theme.fonts.name2;
+      case 'univ1':
+        return theme.fonts.univ1;
+      case 'univ2':
+        return theme.fonts.univ2;
+      case 'point1':
+        return theme.fonts.point1;
+      case 'point2':
+        return theme.fonts.point2;
+      default:
+        return '';
+    }
+  }};
+`;
+
+export default Text;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -19,27 +19,30 @@ interface Font {
   weight: number;
   size: number;
   letterSpacing: number;
+  lineHeight: string;
 }
 
-const FONT = ({ family, weight, size, letterSpacing }: Font): string => {
+const FONT = ({ family, weight, size, letterSpacing, lineHeight }: Font): string => {
   return `
       font-family: ${family};
       font-weight : ${weight};
       font-size : ${size}rem;
       letter-spacing: ${letterSpacing}rem;
+      line-height:${lineHeight}
       `;
 };
 
 const fonts = {
-  head1: FONT({ family: 'Pretendard Variable', weight: 500, size: 2, letterSpacing: -0.03 }),
-  head2: FONT({ family: 'Pretendard Variable', weight: 400, size: 1.4, letterSpacing: -0.03 }),
-  head3: FONT({ family: 'Pretendard Variable', weight: 600, size: 2, letterSpacing: -0.03 }),
-  name1: FONT({ family: 'Pretendard Variable', weight: 400, size: 1.4, letterSpacing: 0 }),
-  name2: FONT({ family: 'Pretendard Variable', weight: 400, size: 1.2, letterSpacing: 0 }),
-  univ1: FONT({ family: 'Pretendard Variable', weight: 400, size: 1.2, letterSpacing: -0.03 }),
-  univ2: FONT({ family: 'Pretendard Variable', weight: 400, size: 1, letterSpacing: -0.03 }),
-  point1: FONT({ family: 'Pretendard Variable', weight: 700, size: 1.8, letterSpacing: 0 }),
-  point2: FONT({ family: 'Pretendard Variable', weight: 500, size: 1.6, letterSpacing: 0 }),
+  head1: FONT({ family: 'Pretendard Variable', weight: 500, size: 2, letterSpacing: -0.03, lineHeight: 'normal' }),
+  head2: FONT({ family: 'Pretendard Variable', weight: 400, size: 1.4, letterSpacing: -0.03, lineHeight: 'normal' }),
+  head3: FONT({ family: 'Pretendard Variable', weight: 600, size: 2, letterSpacing: -0.03, lineHeight: 'normal' }),
+  head4: FONT({ family: 'Pretendard Variable', weight: 700, size: 2.8, letterSpacing: -0.03, lineHeight: 'normal' }),
+  name1: FONT({ family: 'Pretendard Variable', weight: 400, size: 1.4, letterSpacing: 0, lineHeight: 'normal' }),
+  name2: FONT({ family: 'Pretendard Variable', weight: 400, size: 1.2, letterSpacing: 0, lineHeight: 'normal' }),
+  univ1: FONT({ family: 'Pretendard Variable', weight: 400, size: 1.2, letterSpacing: -0.03, lineHeight: 'normal' }),
+  univ2: FONT({ family: 'Pretendard Variable', weight: 400, size: 1, letterSpacing: -0.03, lineHeight: 'normal' }),
+  point1: FONT({ family: 'Pretendard Variable', weight: 700, size: 1.8, letterSpacing: 0, lineHeight: 'normal' }),
+  point2: FONT({ family: 'Pretendard Variable', weight: 500, size: 1.6, letterSpacing: 0, lineHeight: 'normal' }),
 };
 
 export type FontsTypes = typeof fonts;
