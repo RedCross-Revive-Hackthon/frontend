879d02429a438134fb9a7cd73d2f99d4d1491c9b
ljh0608 <m98m@catholic.ac.kr>
2023-08-24 23:03:25 +0900

    design: mypage header

--- src/components/Mypage/MypageHeader.tsx
+++ src/components/Mypage/MypageHeader.tsx
@@ -1,6 +1,6 @@
-import { DefaultProfileImg, MypageBackground } from '../icon/icon';
+import { BtnAlarm, DefaultProfileImg } from '../icon/icon';
 
-import Text from '../Text';
+import Text from '../common/Text';
 import mypageBackgroundImg from '../../assets/svgs/myPageBackgroundImg.png';
 import styled from 'styled-components';
 import { theme } from '../../styles/theme';
@@ -9,21 +9,23 @@ const MypageHeader = () => {
   return (
     <MypageHeaderWrapper>
       <MyInfoWrapper>
-        <DefaultProfileImg></DefaultProfileImg>
         <MyInfoContainer>
-          {' '}
-          <NameWrapper>
-            <Text font={'head3'} color={theme.colors.white}>
-              적십자
-            </Text>
-          </NameWrapper>
-          <MajorWrapper>
-            <Text font={'univ1'} color={theme.colors.white}>
-              적십자 대학교
-            </Text>
-          </MajorWrapper>
-          {/* head4로 추후에 변경해야함 */}
+          <DefaultProfileImg />
+          <MyInfoSection>
+            <NameWrapper>
+              <Text font="head4" color={theme.colors.white}>
+                적십자
+              </Text>
+            </NameWrapper>
+            <MajorWrapper>
+              <Text font={'univ1'} color={theme.colors.white}>
+                적십자 대학교
+              </Text>
+            </MajorWrapper>
+            {/* head4로 추후에 변경해야함 */}
+          </MyInfoSection>
         </MyInfoContainer>
+        <AlarmWrapper></AlarmWrapper>
       </MyInfoWrapper>
     </MypageHeaderWrapper>
   );
@@ -42,20 +44,33 @@ const MypageHeaderWrapper = styled.div`
 
 const MyInfoWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
 `;
 
 const MyInfoContainer = styled.div`
   display: flex;
+`;
+const MyInfoSection = styled.div`
+  display: flex;
   flex-direction: column;
   justify-content: center;
+  margin-left: 1.4rem;
+  width: 15rem;
   height: 7.2rem;
 `;
-const NameWrapper = styled.div`
-  margin-left: 1.4rem;
-`;
+const NameWrapper = styled.div``;
 
 const MajorWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 0.4rem;
   border-radius: 10rem;
   background: rgba(255, 255, 255, 0.5);
+  width: 12rem;
+  height: 2.2rem;
 `;
+
+const AlarmWrapper = styled(BtnAlarm)``;
+
 export default MypageHeader;
