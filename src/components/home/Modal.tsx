5915a1ed234cef859b61abce9be21f5398fd0814
simeunseo <rkdcjfehowl@naver.com>
2023-08-25 01:59:18 +0900

    feat: 모달 컨텐츠

--- src/components/home/Modal.tsx
+++ src/components/home/Modal.tsx
@@ -2,6 +2,7 @@
 
 import { Close } from '../icon/icon';
 import React from 'react';
+import Text from '../common/Text';
 import styled from 'styled-components';
 import { theme } from '../../styles/theme';
 
@@ -13,7 +14,46 @@ const Modal = ({ onClose }: ModalProps) => {
   return (
     <ModalWrapper>
       <Content>
-        <StyledClose onClick={onClose} />
+        <Title>
+          <StyledClose onClick={onClose} />
+          <Text font="body2" color={theme.colors.black}>
+            점수 현황
+          </Text>
+        </Title>
+        <Main>
+          <DetailOneLine>
+            <Text font="body5" color={theme.colors.black}>
+              헌혈
+            </Text>
+            <Text font="body5" color={theme.colors.black}>
+              600점
+            </Text>
+          </DetailOneLine>
+          <DetailOneLine>
+            <Text font="body5" color={theme.colors.black}>
+              헌혈 증서 나눔
+            </Text>
+            <Text font="body5" color={theme.colors.black}>
+              150점
+            </Text>
+          </DetailOneLine>
+          <DetailOneLine>
+            <Text font="body5" color={theme.colors.black}>
+              지정 헌혈
+            </Text>
+            <Text font="body5" color={theme.colors.black}>
+              500점
+            </Text>
+          </DetailOneLine>
+          <DetailOneLine>
+            <Text font="body5" color={theme.colors.black}>
+              커뮤니티
+            </Text>
+            <Text font="body5" color={theme.colors.black}>
+              20점
+            </Text>
+          </DetailOneLine>
+        </Main>
       </Content>
     </ModalWrapper>
   );
@@ -27,6 +67,8 @@ const ModalWrapper = styled.div`
   position: fixed;
   align-items: center;
   justify-content: center;
+
+  padding: 0 2.8rem;
   width: 33.5rem;
 `;
 
@@ -44,4 +86,13 @@ const StyledClose = styled(Close)`
   top: 1.2rem;
   right: 1.2rem;
   cursor: pointer;
+`;
+
+const Title = styled.div``;
+
+const Main = styled.main``;
+
+const DetailOneLine = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
