59d57db67cc6f12aa382ebf545b247acf5f37b72
simeunseo <rkdcjfehowl@naver.com>
2023-08-24 15:28:51 +0900

    feat: icon 관련 세팅

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ThemeTest from '../components/ThemeTest';
+import { ReactComponent as HomeIc } from '../assets/svgs/home.svg';
+import { Icon } from '../components/icon';
+// import { HomeIc } from '../components/icon/icon';
+import { styled } from 'styled-components';
 const Main = () => {
   return (
-    <div>
-      <ThemeTest></ThemeTest>
-    </div>
+    <>
+      왜안떠
+      <Test />
+    </>
   );
 };
 
 export default Main;
+
+const Test = styled(Icon.HomeIc)``;
