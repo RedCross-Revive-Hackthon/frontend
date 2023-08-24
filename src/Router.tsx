09971d447a8c46de0ef0fb2efeda4e05eea98dcb
ljh0608 <m98m@catholic.ac.kr>
2023-08-25 05:25:54 +0900

    feat: instagram sharing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Barcode from './pages/Barcode';
+import Certification from './pages/Certification';
 import Home from './pages/Home';
 import Mypage from './pages/Mypage';
 
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="/barcode" element={<Barcode />}></Route>
         <Route path="/mypage" element={<Mypage></Mypage>}></Route>
+        <Route path="/certification" element={<Certification></Certification>}></Route>
       </Routes>
     </BrowserRouter>
   );
