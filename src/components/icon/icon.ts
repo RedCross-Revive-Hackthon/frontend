18f48801fb7dce4dc982dba9b54ad4e33d4f35f9
simeunseo <rkdcjfehowl@naver.com>
2023-08-24 22:59:28 +0900

    design: 플러스 버튼 위치 조정

--- src/components/icon/icon.ts
+++ src/components/icon/icon.ts
@@ -1,21 +1,25 @@
 import { ReactComponent as BtnInvite } from '../../assets/svgs/btnInvite.svg';
 import { ReactComponent as BtnPoint } from '../../assets/svgs/btnPoint.svg';
 import { ReactComponent as BtnSNS } from '../../assets/svgs/btnSNS.svg';
+import { ReactComponent as Community } from '../../assets/svgs/community.svg';
 import { ReactComponent as DefaultProfileImg } from '../../assets/svgs/defaultProfile.svg';
 import { ReactComponent as DefaultUser } from '../../assets/svgs/defaultUser.svg';
 import { ReactComponent as Down } from '../../assets/svgs/down.svg';
+import { ReactComponent as Event } from '../../assets/svgs/event.svg';
 import { ReactComponent as FirstPlace } from '../../assets/svgs/firstPlace.svg';
-import { ReactComponent as HomeIc } from '../../assets/svgs/home.svg';
+import { ReactComponent as Home } from '../../assets/svgs/home.svg';
 import { ReactComponent as IcnPoint } from '../../assets/svgs/icnPoint.svg';
 import { ReactComponent as IconArea } from '../../assets/svgs/iconArea.svg';
 import { ReactComponent as MyPage } from '../../assets/svgs/myPage.svg';
 import { ReactComponent as MypageBackground } from '../../assets/svgs/mypageBackground.svg';
+import { ReactComponent as PlusBtn } from '../../assets/svgs/PlusBtn.svg';
 import { ReactComponent as SecondPlace } from '../../assets/svgs/secondPlace.svg';
 import { ReactComponent as ThirdPlace } from '../../assets/svgs/thirdPlace.svg';
 import { ReactComponent as Up } from '../../assets/svgs/up.svg';
+import { ReactComponent as Volunteer } from '../../assets/svgs/volunteer.svg';
 
 export {
-  HomeIc,
+  Home,
   DefaultProfileImg,
   MypageBackground,
   BtnInvite,
@@ -30,4 +34,8 @@ export {
   Up,
   Down,
   IconArea,
+  PlusBtn,
+  Community,
+  Event,
+  Volunteer,
 };
