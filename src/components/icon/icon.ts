ec5e86603028010c73c701e48cd322c4c5f161bd
ljh0608 <m98m@catholic.ac.kr>
2023-08-24 23:03:05 +0900

    style: mypage Icon svg 추가

--- src/components/icon/icon.ts
+++ src/components/icon/icon.ts
@@ -1,9 +1,12 @@
+import { ReactComponent as BtnAlarm } from '../../assets/svgs/btnAlarm.svg';
 import { ReactComponent as BtnInvite } from '../../assets/svgs/btnInvite.svg';
 import { ReactComponent as BtnPoint } from '../../assets/svgs/btnPoint.svg';
 import { ReactComponent as BtnSNS } from '../../assets/svgs/btnSNS.svg';
 import { ReactComponent as DefaultProfileImg } from '../../assets/svgs/defaultProfile.svg';
 import { ReactComponent as HomeIc } from '../../assets/svgs/home.svg';
 import { ReactComponent as IcnPoint } from '../../assets/svgs/icnPoint.svg';
+import { ReactComponent as IcnRank } from '../../assets/svgs/icnRank.svg';
+import { ReactComponent as IcnScore } from '../../assets/svgs/icnScore.svg';
 import { ReactComponent as MypageBackground } from '../../assets/svgs/mypageBackground.svg';
 
-export { HomeIc, DefaultProfileImg, MypageBackground, BtnInvite, BtnSNS, BtnPoint, IcnPoint };
+export { HomeIc, DefaultProfileImg, MypageBackground, BtnInvite, BtnSNS, IcnPoint, BtnAlarm, IcnRank, IcnScore };
