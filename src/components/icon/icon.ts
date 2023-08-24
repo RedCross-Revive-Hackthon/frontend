cb11427f44b7c4957983facf2263da054a69f4a9
ljh0608 <m98m@catholic.ac.kr>
2023-08-25 01:28:37 +0900

    chore: button svg

--- src/components/icon/icon.ts
+++ src/components/icon/icon.ts
@@ -7,6 +7,22 @@ import { ReactComponent as HomeIc } from '../../assets/svgs/home.svg';
 import { ReactComponent as IcnPoint } from '../../assets/svgs/icnPoint.svg';
 import { ReactComponent as IcnRank } from '../../assets/svgs/icnRank.svg';
 import { ReactComponent as IcnScore } from '../../assets/svgs/icnScore.svg';
+import { ReactComponent as Instagram } from '../../assets/svgs/instagram.svg';
+import { ReactComponent as Invite } from '../../assets/svgs/invite.svg';
 import { ReactComponent as MypageBackground } from '../../assets/svgs/mypageBackground.svg';
+import { ReactComponent as Trophy } from '../../assets/svgs/trophy.svg';
 
-export { HomeIc, DefaultProfileImg, MypageBackground, BtnInvite, BtnSNS, IcnPoint, BtnAlarm, IcnRank, IcnScore };
+export {
+  HomeIc,
+  DefaultProfileImg,
+  MypageBackground,
+  Instagram,
+  BtnInvite,
+  Invite,
+  Trophy,
+  BtnSNS,
+  IcnPoint,
+  BtnAlarm,
+  IcnRank,
+  IcnScore,
+};
