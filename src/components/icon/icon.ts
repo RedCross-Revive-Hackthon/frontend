0a425afc6bd34efbb6b4e90f66192309c6197304
ljh0608 <m98m@catholic.ac.kr>
2023-08-24 21:24:10 +0900

    chore: svg to reactComponent

--- src/components/icon/icon.ts
+++ src/components/icon/icon.ts
@@ -1,5 +1,9 @@
+import { ReactComponent as BtnInvite } from '../../assets/svgs/btnInvite.svg';
+import { ReactComponent as BtnPoint } from '../../assets/svgs/btnPoint.svg';
+import { ReactComponent as BtnSNS } from '../../assets/svgs/btnSNS.svg';
 import { ReactComponent as DefaultProfileImg } from '../../assets/svgs/defaultProfile.svg';
 import { ReactComponent as HomeIc } from '../../assets/svgs/home.svg';
+import { ReactComponent as IcnPoint } from '../../assets/svgs/icnPoint.svg';
 import { ReactComponent as MypageBackground } from '../../assets/svgs/mypageBackground.svg';
 
-export { HomeIc, DefaultProfileImg, MypageBackground };
+export { HomeIc, DefaultProfileImg, MypageBackground, BtnInvite, BtnSNS, BtnPoint, IcnPoint };
