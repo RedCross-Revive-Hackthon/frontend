e3c09e41dc52bc5c75597b32bd66fa3374db3bb5
simeunseo <rkdcjfehowl@naver.com>
2023-08-24 15:31:10 +0900

    feat: svgr plugin setting

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,16 +1,14 @@
-import { ReactComponent as HomeIc } from '../assets/svgs/home.svg';
 import { Icon } from '../components/icon';
-// import { HomeIc } from '../components/icon/icon';
 import { styled } from 'styled-components';
+
 const Main = () => {
   return (
     <>
-      왜안떠
-      <Test />
+      <IconTest />
     </>
   );
 };
 
 export default Main;
 
-const Test = styled(Icon.HomeIc)``;
+const IconTest = styled(Icon.HomeIc)``;
